542b435abe6a93e4343091acdce37c6a38369966
Daniel Andrade <andrade.d@live.com>
2021-11-20 19:19:36 -0300

    Ajustes

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 # Docker-compose install
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+#sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+# Create network
+sudo docker network create --attachable --driver overlay proxy
