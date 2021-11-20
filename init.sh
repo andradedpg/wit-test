075e782e113c67a9245a2265c9f607e305a37a1a
Daniel Andrade <andrade.d@live.com>
2021-11-20 18:08:24 -0300

    Ajustes

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
 # Docker-compose install
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# ...
