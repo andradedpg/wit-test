82c1341fcd2f05f72daac7cb1abfe405fc3a7b7a
Daniel Andrade <andrade.d@live.com>
2021-11-21 18:14:03 -0300

    SSL Self Signed

--- restart.sh
+++ restart.sh
@@ -1,1 +1,1 @@
-docker stack rm wit && sleep 3 && docker stack deploy --compose-file docker-compose.yml wit
+docker stack rm wit && sleep 3000 && docker stack deploy --compose-file docker-compose.yml wit
